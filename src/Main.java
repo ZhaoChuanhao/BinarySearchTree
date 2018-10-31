bcbde3ee25fc2de2576fd1a904b3675fcaf05cf6
赵传昊 <724356121@qq.com>
2018-10-31 23:03:11 +0800

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        BST<Integer> bst = new BST<Integer>();
+        BST<Integer> bst = new BST<>();
         int[] nums = {5, 3, 6, 8, 4, 2};
         for (int i = 0; i < nums.length; i++){
             bst.add(nums[i]);
